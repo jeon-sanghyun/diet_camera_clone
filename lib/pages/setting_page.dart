bc506746c0b9c3083c99f1b871bccf076126cbcf
sang hyun Jeon <jsh0813@gmail.com>
2020-03-11 12:27:07 +0900

    -. setting_page listview 생성, leading 이미지 삽입, trailing

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -6,11 +6,24 @@ class SettingPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: Text('setting'),
-
+        title: Text(
+          'setting',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
       ),
       body: ListView(
-        children: <Widget>[],
+        children: <Widget>[
+      ListTile(title: Text('profile'),),
+          ListTile(leading: Icon(Icons.face) ,title: Text('목표 kcal'),trailing: Text('aaaaa'),onTap: null,),
+          ListTile(leading: Image.asset("./assets/search.png"), title: Text('단'),),
+          ListTile(title: Text('활동량'),),
+          ListTile(title: Text('저장옵션'),),
+          ListTile(title: Text('프리미엄'),),
+          ListTile(title: Text('알림'),),
+          ListTile(title: Text('도움'),),
+        ],
       ),
     );
   }
