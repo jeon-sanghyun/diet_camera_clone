a3a49998ec42fb08ab2e69b98d9fd897691e7bfb
sang hyun Jeon <jsh0813@gmail.com>
2020-03-11 14:44:47 +0900

    -.setting_page profile image 삽입, grid삽입,

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -15,14 +15,107 @@ class SettingPage extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
-      ListTile(title: Text('profile'),),
-          ListTile(leading: Icon(Icons.face) ,title: Text('목표 kcal'),trailing: Text('aaaaa'),onTap: null,),
-          ListTile(leading: Image.asset("./assets/search.png"), title: Text('단'),),
-          ListTile(title: Text('활동량'),),
-          ListTile(title: Text('저장옵션'),),
-          ListTile(title: Text('프리미엄'),),
-          ListTile(title: Text('알림'),),
-          ListTile(title: Text('도움'),),
+         Container(
+           child: Padding(
+             padding: const EdgeInsets.all(50.0),
+             child: Column(
+               children: <Widget>[
+                 Container(
+                   width: 150,
+                   height: 150,
+                   child:
+                   Image.asset("./assets/home.png", fit: BoxFit.fill,),
+                 ),
+
+                 Text('name'),
+                 Text('modify'),
+               ],
+             ),
+           ),
+         ),
+          Divider(
+            thickness: 2,
+            color: Colors.grey[200],
+          ),
+          ListTile(
+            leading: Image.asset(
+              "./assets/home.png",
+              width: 30,
+            ),
+            title: Text('목표 kcal'),
+            trailing: Text('aaaaa'),
+            onTap: null,
+          ),
+          Divider(
+            thickness: 2,
+            color: Colors.grey[200],
+          ),
+          ListTile(
+            leading: Image.asset(
+              "./assets/search.png",
+              width: 30,
+            ),
+            title: Text('단'),
+          ),
+          Divider(
+            thickness: 2,
+            color: Colors.grey[200],
+          ),
+          ListTile(
+            leading: Image.asset(
+              "./assets/like.png",
+              width: 30,
+            ),
+            title: Text('활동량'),
+          ),
+          Divider(
+            thickness: 2,
+            color: Colors.grey[200],
+          ),
+          ListTile(
+            leading: Image.asset(
+              "./assets/profile.png",
+              width: 30,
+            ),
+            title: Text('저장옵션'),
+          ),
+          Divider(
+            thickness: 2,
+            color: Colors.grey[200],
+          ),
+          ListTile(
+            leading: Image.asset(
+              "./assets/saved.png",
+              width: 30,
+            ),
+            title: Text('프리미엄'),
+          ),
+          Divider(
+            thickness: 2,
+            color: Colors.grey[200],
+          ),
+          ListTile(
+            leading: Image.asset(
+              "./assets/comment.png",
+              width: 30,
+            ),
+            title: Text('알림'),
+          ),
+          Divider(
+            thickness: 2,
+            color: Colors.grey[200],
+          ),
+          ListTile(
+            leading: Image.asset(
+              "./assets/heart.png",
+              width: 30,
+            ),
+            title: Text('도움'),
+          ),
+          Divider(
+            thickness: 2,
+            color: Colors.grey[200],
+          ),
         ],
       ),
     );
